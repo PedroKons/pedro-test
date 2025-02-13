38aed6d71b28511876c0c8665b019ab307293cea
pedro kons <pedro_kons@edu.univali.br>
2025-02-13 20:11:58 -0300

    update port

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const Fastify = require('fastify');
 const fastify = Fastify({ logger: true });
 
+const PORT = process.env.PORT || 5000;  // Alterado para 3001
+const HOST = '0.0.0.0';
+
 fastify.get('/ping', async (request, reply) => {
   return { message: 'pong' };
 });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Server is running on http://0.0.0.0:3000');
+    await fastify.listen({ port: PORT, host: HOST });
+    console.log(`Server is running on http://${HOST}:${PORT}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
